86349e82213e3e22b6666e1b0b8f59f17c4c37e1
Manish Punia <mkpunia92@gmail.com>
2020-11-24 21:59:04 +0530

    Update binary-tree-path-sum.swift

--- dfs/binary-tree-path-sum.swift
+++ dfs/binary-tree-path-sum.swift
@@ -1,4 +1,5 @@
-  public class TreeNode {
+// https://leetcode.com/problems/path-sum
+public class TreeNode {
       public var val: Int
       public var left: TreeNode?
       public var right: TreeNode?
@@ -23,5 +24,48 @@ class Solution {
             return true
         }
         return hasPathSum(root?.left, sum - root!.val) || hasPathSum(root?.right, sum - root!.val)
+    }
+  
+  func hasPathSumIterative(_ root: TreeNode?, _ sum: Int) -> Bool {
+        
+        var nr = root
+        if nr == nil
+        {
+            return false
+        }
+        
+        var s = stack()
+        var currentSum: Int = 0
+        var last:TreeNode? = nil
+        
+        while nr != nil || s.isEmpty() == false
+        {
+            while nr != nil
+            {
+                currentSum = currentSum + nr!.val
+                s.push(nr)
+                nr = nr?.left
+            }
+            
+            nr = s.top()
+            if nr?.left == nil && nr?.right == nil && currentSum == sum
+            {
+                return true
+            }
+            
+            if nr?.right != nil && nr?.right != last 
+            {
+                nr = nr?.right
+            }
+            else
+            {
+                nr = s.pop()
+                last = nr
+                currentSum = currentSum - nr!.val
+                nr = nil
+            }
+        }
+        return false
+        
     }
 }
